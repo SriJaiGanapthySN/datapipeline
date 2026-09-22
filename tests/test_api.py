580a4eca4351424865fc5bb0cdf9d8681e8b7e16
S.N <sns1@medtronic.com>
2026-09-23 01:44:58 +0530

    added testing

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,23 @@
+from unittest.mock import patch, Mock
 from orchestrator.api_extractor import get_movie
 
-movie = get_movie(550)
 
-print(movie["title"])
-print(movie["release_date"])
-print(movie["vote_average"])
+@patch("orchestrator.api_extractor.requests.get")
+def test_get_movie(mock_get):
+
+    fake_response = Mock()
+    fake_response.status_code = 200
+    fake_response.json.return_value = {
+        "id": 550,
+        "title": "Fight Club",
+        "runtime": 139,
+        "vote_average": 8.8
+    }
+
+    mock_get.return_value = fake_response
+
+    movie = get_movie(550)
+
+    assert movie["id"] == 550
+    assert movie["title"] == "Fight Club"
+    assert movie["runtime"] == 139
