4a3a8cc6a70fa393b4bb7cccabfa9314eca6ffdf
S.N <sns1@medtronic.com>
2026-09-23 22:12:27 +0530

    test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,26 +1,22 @@
-from unittest.mock import patch, Mock
+from unittest.mock import Mock, patch
+
 from orchestrator.api_extractor import get_movie
 
 
-@patch("orchestrator.api_extractor.session.get")
+@patch("orchestrator.api_extractor.requests.get")
 def test_get_movie(mock_get):
-
-    fake_response = Mock()
-    fake_response.status_code = 200
-    fake_response.json.return_value = {
+    mock_response = Mock()
+    mock_response.json.return_value = {
         "id": 550,
-        "title": "Fight Club",
-        "runtime": 139,
-        "vote_average": 8.8
+        "title": "Fight Club"
     }
+    mock_response.raise_for_status.return_value = None
 
-    mock_get.return_value = fake_response
+    mock_get.return_value = mock_response
 
     movie = get_movie(550)
 
     assert movie["id"] == 550
     assert movie["title"] == "Fight Club"
-    assert movie["runtime"] == 139
-    assert movie["vote_average"] == 8.8
 
     mock_get.assert_called_once()
